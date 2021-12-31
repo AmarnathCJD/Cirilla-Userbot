b9339fe19c8d87994c8b76f22721160c6ae9940a
Amarnath C <41010786@sfcollege.edu>
2021-12-31 22:42:22 +0530

    Create eval.py

---
+++ ciri/modules/eval.py
@@ -0,0 +1,57 @@
+from ciri.utils import ciri_cmd, eor
+import sys, traceback, io
+
+
+
+@ciri_cmd(pattern="eval", full_sudo=True)
+async def eval__(e):
+    try:
+        a = e.text.split(maxsplit=1)[1]
+    except IndexError:
+        return await e.edit("`Give some python cmd`")
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    ros = sys.stdout = io.StringIO()
+    red = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(a, e)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = ros.getvalue()
+    stderr = red.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = (
+        "__►__ **EVALPy**\n```{}``` \n\n __►__ **OUTPUT**: \n```{}``` \n".format(
+            a,
+            evaluation,
+        )
+    )
+    if len(evaluation) > 4095:
+        with io.BytesIO(evaluation.encode()) as finale_b:
+            finale_b.name = "eval.txt"
+            return await eor(e, f"```{c}```", file=finale_b)
+    await eor(e, final_output)
+
+
+async def aexec(code, event):
+    exec(
+        f"async def __aexec(e, client): "
+        + "\n message = event = e"
+        + "\n reply = await event.get_reply_message()"
+        + "\n p = print
+        + "".join(f"\n {l}" for l in code.split("\n")),
+    )
+
+    return await locals()["__aexec"](event, event.client)
+
+
