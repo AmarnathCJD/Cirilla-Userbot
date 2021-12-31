62a41bb93eed1532bcb8bb76d6e8ad8fbd432c9e
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-12-31 15:43:35 +0000

    RoseLoverX: auto linting

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 json = {
- "name": "Cirilla Userbot",
- "description": "A lightning fast telegram personal userbot.",
- "logo": "https://te.legra.ph/file/ec8ce6c21a96c887bb8de.jpg",
- "keywords": ["Telegram", "Userbot", "Fast", "Team RLX", "Telethon"],
- "repository": "https://github.com/AmarnathCJD/Cirilla-Userbot",
- "env": {"API_KEY": {"description": "App ID get from my.telegram.org", "value": ""}, "API_HASH": {"description": "Api Hash value get from my.telegram.org", "value": ""}, "STRING_SESSION": {"description": "Telegram user string session", "value": ""}, "MONGO_DB_URL": {"description": "Database url get from mongodb.com", "value": "", }},
- "buildpacks": [{"url": "heroku/python"}]
+    "name": "Cirilla Userbot",
+    "description": "A lightning fast telegram personal userbot.",
+    "logo": "https://te.legra.ph/file/ec8ce6c21a96c887bb8de.jpg",
+    "keywords": ["Telegram", "Userbot", "Fast", "Team RLX", "Telethon"],
+    "repository": "https://github.com/AmarnathCJD/Cirilla-Userbot",
+    "env": {
+        "API_KEY": {"description": "App ID get from my.telegram.org", "value": ""},
+        "API_HASH": {
+            "description": "Api Hash value get from my.telegram.org",
+            "value": "",
+        },
+        "STRING_SESSION": {"description": "Telegram user string session", "value": ""},
+        "MONGO_DB_URL": {
+            "description": "Database url get from mongodb.com",
+            "value": "",
+        },
+    },
+    "buildpacks": [{"url": "heroku/python"}],
 }
