d51d456c24d2b93c73eaf95375d97d22187eebe4
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-12-31 16:13:44 +0000

    RoseLoverX: auto linting

--- ciri/__main__.py
+++ ciri/__main__.py
@@ -1,13 +1,14 @@
-from ciri.utils import load_modules
-from ciri import ub, bot
 import sys
 
+from ciri import bot, ub
+from ciri.utils import load_modules
+
 try:
-  bot.start(bot_token=BOT_TOKEN)
-  ub.start()
+    bot.start(bot_token=BOT_TOKEN)
+    ub.start()
 except Exception as exc:
-  print(exc)
-  sys.exit(1)
+    print(exc)
+    sys.exit(1)
 
 load_modules()
 Print("Ciri Userbot Is Alive")
