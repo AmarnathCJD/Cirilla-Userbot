48e72519e5604e356aa639e514f6747c512fb737
Amarnath C <41010786@sfcollege.edu>
2021-12-31 21:43:28 +0530

    Update __main__.py

--- ciri/__main__.py
+++ ciri/__main__.py
@@ -1,1 +1,15 @@
+from ciri.utils import load_modules
+from ciri import ub, bot
+import sys
 
+try:
+  bot.start(bot_token=BOT_TOKEN)
+  ub.start()
+except Exception as exc:
+  print(exc)
+  sys.exit(1)
+
+load_modules()
+Print("Ciri Userbot Is Alive")
+
+ub.run_until_disconnected()
