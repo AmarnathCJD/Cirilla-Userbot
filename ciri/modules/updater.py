d004172b74b594e3eb5351f927aa13f085c1043f
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-26 20:13:55 +0530

    Update updater.py

--- ciri/modules/updater.py
+++ ciri/modules/updater.py
@@ -1,13 +1,20 @@
+# RoseLoverX (©)
+
 import asyncio
 import sys
-from os import environ, execle
+from os import environ, execle, path, remove
 
-import git
-from git.exc import GitCommandError
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from ciri.utils import ciri_cmd
+from ciri.utils import ciri_cmd, eor
 
-REPO_URL = "https://github.com/amarnathcjd/ciri-userbot.git"
+UPSTREAM_REPO_BRANCH = "master"
+UPSTREAM_REPO_URL = "https://github.com/amarnathcjd/ciri-userbot"
+
+requirements_path = path.join(
+    path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
+)
 
 
 async def gen_chlog(repo, diff):
@@ -20,8 +27,31 @@ async def gen_chlog(repo, diff):
     return ch_log
 
 
+async def print_changelogs(event, ac_br, changelog):
+    changelog_str = (
+        f"**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`"
+    )
+    if len(changelog_str) > 4096:
+        await event.edit("`Changelog is too big, view the file to see it.`")
+        with open("output.txt", "w+") as file:
+            file.write(changelog_str)
+        await event.client.send_file(
+            event.chat_id,
+            "output.txt",
+            reply_to=event.id,
+        )
+        remove("output.txt")
+    else:
+        await event.client.send_message(
+            event.chat_id,
+            changelog_str,
+            reply_to=event.id,
+        )
+    return True
+
+
 async def update_requirements():
-    reqs = "requirements.txt"
+    reqs = str(requirements_path)
     try:
         process = await asyncio.create_subprocess_shell(
             " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
@@ -44,30 +74,67 @@ async def update(event, repo, ups_rem, ac_br):
         "`Successfully Updated!\n" "Bot is restarting... Wait for a minute!`"
     )
     # Spin a new instance of bot
-    args = [sys.executable, "-m", "userbot"]
+    args = [sys.executable, "-m", "ciri"]
     execle(sys.executable, *args, environ)
     return
 
 
-@ciri_cmd(pattern="update")
-async def update_ub(e):
-    repo = git.Repo()
+@ciri_cmd(pattern="update", allow_sudo=True)
+async def upstream(event):
+    "For .update command, check if the bot is up to date, update if specified"
+    conf = event.pattern_match.group(1).strip()
+    event = await eor(event, "`Checking for updates, please wait....`")
+    off_repo = UPSTREAM_REPO_URL
+    force_update = False
     try:
-        repo.create_remote("upstream", REPO_URL)
+        txt = "`Oops.. Updater cannot continue due to "
+        txt += "some problems occured`\n\n**LOGTRACE:**\n"
+        repo = Repo()
+    except NoSuchPathError as error:
+        await event.edit(f"{txt}\n`directory {error} is not found`")
+        return repo.__del__()
+    except GitCommandError as error:
+        await event.edit(f"{txt}\n`Early failure! {error}`")
+        return repo.__del__()
+    except InvalidGitRepositoryError as error:
+        if conf is None:
+            return await event.edit(
+                f"`Unfortunately, the directory {error} "
+                "does not seem to be a git repository.\n"
+                "But we can fix that by force updating the userbot using "
+                ".update now.`"
+            )
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        force_update = True
+        repo.create_head("master", origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
+    ac_br = repo.active_branch.name
+    if ac_br != UPSTREAM_REPO_BRANCH:
+        await event.edit(
+            "**[UPDATER]:**\n"
+            f"`Looks like you are using your own custom branch ({ac_br}). "
+            "in that case, Updater is unable to identify "
+            "which branch is to be merged. "
+            "please checkout to any official branch`"
+        )
+        return repo.__del__()
+    try:
+        repo.create_remote("upstream", off_repo)
     except BaseException:
         pass
     ups_rem = repo.remote("upstream")
-    ac_br = repo.active_branch.name
     ups_rem.fetch(ac_br)
     changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-    if conf == "deploy":
-        await event.edit("`Deploying userbot, please wait....`")
-        return
     if changelog == "" and not force_update:
-        await event.edit("\n`Ciri has no new updates with  " f"**master**\n")
+        await event.edit(
+            "\n`Ciri has no new updates with  " f"**{UPSTREAM_REPO_BRANCH}**\n"
+        )
         return repo.__del__()
     if conf == "" and not force_update:
-        await e.edit(gen_changelog)
+        await print_changelogs(event, ac_br, changelog)
         await event.delete()
         return await event.respond(
             'do "[`.update now`] or [`.update deploy`]" to update.Check `.help updater` for details'
@@ -81,3 +148,6 @@ async def update_ub(e):
         await event.edit("`Updating userbot, please wait....`")
         await update(event, repo, ups_rem, ac_br)
     return
+
+
+
