5eba467694fac9a5b3cb59f80d30bd260f63a460
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-26 20:51:24 +0530

    Update autodp.py

--- ciri/modules/autodp.py
+++ ciri/modules/autodp.py
@@ -10,13 +10,11 @@ PICS = []
 import io
 import logging
 
-OK = []
 from requests import get
 
 
 @ciri_cmd(pattern="autodp", allow_sudo=True)
 async def _auto_dp(e):
-    global AUTO_DP, PICS
     try:
         args = e.text.split(maxsplit=1)[1]
     except IndexError:
@@ -24,22 +22,24 @@ async def _auto_dp(e):
     if args == "off":
         AUTO_DP = False
         await eor(e, "AutoDP has been disabled.")
+        PICS.clear()
         return
     search = image_dl(args, limit=80)
     if search == None:
         return await eor(e, "No results found for search '{}'.".format(args))
-    PICS = search
-    await eor(e, "AutoDP has been started with keyword '{}'.".format(args))
-    chance = -1
+    for x in search:
+        PICS.append(x)
+    await eor(e, "AutoDP has been started with keyword '{}'💗.".format(args))
     AUTO_DP = True
-    print(len(PICS) > (chance + 1))
-    while AUTO_DP and len(PICS) > (chance + 1):
+    chance = 0
+    while AUTO_DP and len(PICS) != 0:
         try:
-            chance += 1
             file_fetch = get(PICS[chance]).content
             with io.BytesIO(file_fetch.content) as b:
-                b.name = "wall.jpg"
+                b.name = "pic.jpg"
                 file = await e.client.upload_file(b)
+            PICS.pop(chance)
+            chance += 1
             await e.client(functions.photos.UploadProfilePhotoRequest(file))
         except Exception as exception:
             logging.info(exception)
