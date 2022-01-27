e3636cf820657fd951c82476c54210d77fec4020
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-01-27 16:50:11 +0000

    RoseLoverX: auto linting

--- ciri/modules/_start.py
+++ ciri/modules/_start.py
@@ -3,7 +3,7 @@ from ciri.utils import ciri_cmd, eor
 
 ALIVE_PIC = []
 
-ALIVE_CAPTION = '''
+ALIVE_CAPTION = """
 <b>The Ciri Userbot...</b>
 
 <b>Hey,  I am alive.</b>
@@ -14,7 +14,8 @@ ALIVE_CAPTION = '''
 <b>✵ Python -</b> <code>3.9.9</code>
 <b>✵ Telethon -</b> </code>1.25.0</code>
 <b>✵ Branch -</b>  <a href='github.com/amarnathcjd/cirilla-userbot'>master</a>
-'''
+"""
+
 
 @ciri_cmd(pattern="alive", allow_sudo=True)
 async def _start(e):
@@ -22,10 +23,10 @@ async def _start(e):
     image = None
     await e.delete()
     if len(ALIVE_PIC) == 1:
-       image = ALIVE_PIC[0][1]
-       if ALIVE_PIC[0][0] == "sticker":
-          image = None
-          await e.respond(file=ALIVE_PIC[0][1])
+        image = ALIVE_PIC[0][1]
+        if ALIVE_PIC[0][0] == "sticker":
+            image = None
+            await e.respond(file=ALIVE_PIC[0][1])
     await e.respond(
         ALIVE_TEXT.format(me.first_name),
         file=image,
@@ -33,17 +34,39 @@ async def _start(e):
         link_preview=False,
     )
 
+
 @ciri_cmd(pattern="setalivepic", allow_sudo=True)
 async def set_dp(e):
- if not e.reply_to and not (await e.get_reply_message()).media:
-    return await eor(e, "`Reply to image to set alive pic!`")
- r = await e.get_reply_message()
- if not r.photo and not r.sticker:
-    return await eor(e, "Thats not a valid sticker or image.")
- if r.sticker:
-    ALIVE_PIC.clear()
-    ALIVE_PIC.append(["sticker", types.Document(id=r.document.id, access_hash=r.document.access_hash, file_reference=r.document.file_reference)])
- else:
-    ALIVE_PIC.clear()
-    ALIVE_PIC.append(["photo", types.Photo(id=r.photo.id, access_hash=r.photo.access_hash, file_reference=r.photo.file_reference, sizes=r.photo.sizes, attributes=[], dc_id=r.photo.dc_id)])
- await eor(e, "sucessfully set custom alive pic.")
+    if not e.reply_to and not (await e.get_reply_message()).media:
+        return await eor(e, "`Reply to image to set alive pic!`")
+    r = await e.get_reply_message()
+    if not r.photo and not r.sticker:
+        return await eor(e, "Thats not a valid sticker or image.")
+    if r.sticker:
+        ALIVE_PIC.clear()
+        ALIVE_PIC.append(
+            [
+                "sticker",
+                types.Document(
+                    id=r.document.id,
+                    access_hash=r.document.access_hash,
+                    file_reference=r.document.file_reference,
+                ),
+            ]
+        )
+    else:
+        ALIVE_PIC.clear()
+        ALIVE_PIC.append(
+            [
+                "photo",
+                types.Photo(
+                    id=r.photo.id,
+                    access_hash=r.photo.access_hash,
+                    file_reference=r.photo.file_reference,
+                    sizes=r.photo.sizes,
+                    attributes=[],
+                    dc_id=r.photo.dc_id,
+                ),
+            ]
+        )
+    await eor(e, "sucessfully set custom alive pic.")
