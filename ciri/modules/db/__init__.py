793d52e788ae66b54c0b9822d2c594c34b075934
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-12 21:05:38 +0530

    Create __init__.py

---
+++ ciri/modules/db/__init__.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+
+DB_URL = getenv("MONGO_DB_URI")
+
+from pymongo import MongoClient
+
+DB = MongoClient(DB_URL)["ciri"]
+
+def set_dp(id, access_hash, file_reference):
+ DB.main.update_one({"id": "dp"}, {"$set": {"id": id, "acess_hash": access_hash, "file_reference": file_reference}}, upsert=True)
+
+def get_dp():
+ return DB.main.find_one({"id": "dp"})
