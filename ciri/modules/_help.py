c1b84bb80e4223724fe57636008057edd9548258
Amarnath C <amarnathcharichilil@gmail.com>
2022-03-03 22:31:47 +0530

    Update _help.py

--- ciri/modules/_help.py
+++ ciri/modules/_help.py
@@ -18,7 +18,7 @@ async def help_menu(e):
         btn.append(Button.inline(x, x.lower()))
     if len(btn) != 0:
         buttons.append(btn)
-    result = e.builder.article(title=string, text=text, buttons=buttons)
+    result = e.builder.article(title=string, text=string, buttons=buttons)
     await e.answer([result])
 
 
