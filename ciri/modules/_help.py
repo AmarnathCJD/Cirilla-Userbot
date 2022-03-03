b68c9785e915fb791e0f9ea678b48ee87bd7d66e
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-03-03 16:55:01 +0000

    RoseLoverX: auto linting

--- ciri/modules/_help.py
+++ ciri/modules/_help.py
@@ -1,7 +1,7 @@
 from .. import bot
 
 cmds = ["Alive", "Autodp", "Eval", "Spotdl"]
-from telethon import events, functions, Button
+from telethon import Button, events, functions
 
 from ..utils import ciri_cmd, eor
 
@@ -12,32 +12,30 @@ async def help_menu(e):
     buttons = []
     btn = []
     for x in cmds:
-       if len(btn) == 2:
-          buttons.append(btn)
-          btn = []
-       btn.append(Button.Inline(x, x.lower()))
+        if len(btn) == 2:
+            buttons.append(btn)
+            btn = []
+        btn.append(Button.Inline(x, x.lower()))
     if len(btn) != 0:
-      buttons.append(btn)
+        buttons.append(btn)
     result = await e.builder.article(string, buttons=buttons)
     await e.answer([result])
 
+
 @ciri_cmd(pattern="help")
 async def help_show(e):
- bot_get = await bot.get_me()
- r = await e.client.inline_query("@" + bot_get.username, "help")
- await r[0].click(
-                e.chat_id, reply_to=e.reply_to_msg_id, hide_via=True
-            )
- await e.delete()
+    bot_get = await bot.get_me()
+    r = await e.client.inline_query("@" + bot_get.username, "help")
+    await r[0].click(e.chat_id, reply_to=e.reply_to_msg_id, hide_via=True)
+    await e.delete()
+
 
 @ciri_cmd(pattern="dc")
 async def _(e):
     if e.fwd_from:
         return
     result = await e.client(functions.help.GetNearestDcRequest())
-    res = (
-        "**Current DC:** {}\n**Nearest DC:** {}\n**Country:** {}".format(
-            result.this_dc, result.nearest_dc, result.country
-        )
+    res = "**Current DC:** {}\n**Nearest DC:** {}\n**Country:** {}".format(
+        result.this_dc, result.nearest_dc, result.country
     )
     await eor(e, res)
