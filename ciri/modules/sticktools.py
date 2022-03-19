d6149db4295ec7d1629df2566824852882275a8f
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-03-19 17:04:15 +0000

    RoseLoverX: auto linting

--- ciri/modules/sticktools.py
+++ ciri/modules/sticktools.py
@@ -1,12 +1,14 @@
-from ciri.utils import ciri_cmd, eor
 import os
+
+from ciri.utils import ciri_cmd, eor
+
 
 @ciri_cmd(pattern="stoi")
 async def _stkr_to_img(e):
- r = await e.get_reply_message()
- if not r and not r.sticker:
-    return await eor(e, "`Thats not a sticker.`")
- stk = await r.download_media()
- os.rename(skt, stk.replace('.webp', '.jpg'))
- await e.respond(file=stk.replace('.webp', '.jpg'))
- os.remove(stk.replace('.webp', '.jpg'))
+    r = await e.get_reply_message()
+    if not r and not r.sticker:
+        return await eor(e, "`Thats not a sticker.`")
+    stk = await r.download_media()
+    os.rename(skt, stk.replace(".webp", ".jpg"))
+    await e.respond(file=stk.replace(".webp", ".jpg"))
+    os.remove(stk.replace(".webp", ".jpg"))
