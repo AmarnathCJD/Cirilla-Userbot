d7663394c9b38869ea36efe1d833c0ef2884f226
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-03-20 10:19:24 +0000

    RoseLoverX: auto linting

--- ciri/modules/db/__init__.py
+++ ciri/modules/db/__init__.py
@@ -4,6 +4,7 @@ DB_URL = getenv("MONGO_DB_URL")
 
 from pymongo import MongoClient
 from telethon import types
+
 DB = MongoClient(DB_URL)["ciri"]
 
 
@@ -27,4 +28,8 @@ def get_dp():
     if x:
         return x
     else:
-        return types.Document(id=5888843716973562956, access_hash=8845595446721316165, file_reference=b'\x04H\xdeIR\x00\x06I\xc8b6\xfd\xfe\xb1~\xcd\xc9\xc8\xa7-\x8f\xa0\xbc`\x7f\xd6\xef\xee\x19')
+        return types.Document(
+            id=5888843716973562956,
+            access_hash=8845595446721316165,
+            file_reference=b"\x04H\xdeIR\x00\x06I\xc8b6\xfd\xfe\xb1~\xcd\xc9\xc8\xa7-\x8f\xa0\xbc`\x7f\xd6\xef\xee\x19",
+        )
