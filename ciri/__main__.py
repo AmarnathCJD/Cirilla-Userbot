c443b6ff3768b3530dd6d2bb8db3ad4a386abb6c
Amarnath C <amarnathcharichilil@gmail.com>
2022-03-24 13:23:32 +0530

    Update __main__.py

--- ciri/__main__.py
+++ ciri/__main__.py
@@ -10,7 +10,6 @@ except Exception:
     sys.exit(1)
 
 userbot.loop.run_until_complete(get_owner())
-print("Ji", OWNER_ID)
 load_modules()
-print("Cirilla Userbot Is Alive")
+print("cirilla Userbot Is Alive")
 userbot.run_until_disconnected()
